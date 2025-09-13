54bdcfaaddf96d3e17d525c1cbc9fe155e1fdb5b
Philip Spitzlinger <anardil@gmx.at>
2025-09-13 21:20:16 +0200

    Added helper scripts.

---
+++ scripts/flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DEV=/dev/${1:-sda1}
+echo "Boot into Bootloader ..."
+while [ ! -e ${DEV} ]; do
+	echo "Waiting for ${DEV} to appear ..."
+	sleep 5
+done
+sudo mount ${DEV} ./keeb-ce-mount
+sudo cp qmk_firmware/.build/keebio_iris_ce_rev1_FirstTry.uf2 ./keeb-ce-mount/
+sudo umount ./keeb-ce-mount
---
+++ scripts/rebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+qmk json2c iris-ce.json > qmk_firmware/keyboards/keebio/iris_ce/keymaps/FirstTry/keymap.c
+qmk compile -km FirstTry
