075cfd0080ecfc3ee8ea5b6e2ae5238e5a69c45a
Philip Spitzlinger <anardil@gmx.at>
2025-09-19 11:06:08 +0200

    Refined keymap and flashing script.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -5,6 +5,9 @@ while [ ! -e ${DEV} ]; do
 	echo "Waiting for ${DEV} to appear ..."
 	sleep 5
 done
+if [ ! -e ./keeb-ce-mount ]; then 
+	mkdir ./keeb-ce-mount
+fi
 sudo mount ${DEV} ./keeb-ce-mount
 sudo cp qmk_firmware/.build/keebio_iris_ce_rev1_FirstTry.uf2 ./keeb-ce-mount/
 sudo umount ./keeb-ce-mount
